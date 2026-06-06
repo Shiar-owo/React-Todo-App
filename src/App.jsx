1d8e003af00f4eb99496f1cea7200e7e7c746282
Marcelo <marcelo.gabriel.llerena@gmail.com>
2026-06-05 20:23:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { Counter } from "./components/counter/counter";
+
+export function App() {
+  return <Counter />;
+}
---
+++ src/components/counter/counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import styles from "./counter.module.css";
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const isDisabled = counter === 0;
+
+  return (
+    <div className={styles.container}>
+      <p>{counter}</p>
+
+      <button className={styles.button} onClick={handleIncrement}>
+        Incrementar
+      </button>
+
+      <button
+        disabled={isDisabled}
+        onClick={handleDecrement}
+        className={`${styles.button} ${isDisabled && styles.disabled}`}
+      >
+        Decrementar
+      </button>
+    </div>
+  );
+}
