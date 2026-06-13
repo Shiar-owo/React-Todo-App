eb5495ea64104e4bc407e2f5bdda5157d4fdaf9c
Shiar owo <jhairtoc@gmail.com>
2026-06-12 19:57:48 -0500

    refactor: remove unused counter, clock, and context modules

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import "./styles/global.css";
 import { App } from "./App.jsx";
-import { CounterProvider } from "./context/counter-provider/counter-provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CounterProvider>
-      <App />
-    </CounterProvider>
+    <App />
   </StrictMode>,
 );
